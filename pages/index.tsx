d95309958d46c3d8f7429c3e580b044154bacac5
Juan Ignacio Londra <londrajuan@gmail.com>
2023-04-21 20:31:27 -0300

    styling cohesion h3 h2

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,19 @@ import { ChangeEvent, useState } from "react";
 import { Persona } from "../utils/interfaces";
 import { PersonasSchemaValidation } from "../utils/validation";
 import useNotification from "../lib/useSnackbar";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import { buildExcelPersonas, buildNormalizedExcel } from "../utils/xlsx";
+import Divider from '@mui/material/Divider';
 //@ts-ignore
 import * as XLSX from "xlsx-js-style";
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Typography, Box } from "@mui/material";
-import cellImage from "../public/assets/cell_format.png"
+import { Typography, Box } from "@mui/material";
+import cellImage from "../public/assets/cell_format.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [msg, sendNotification] = useNotification();
-
 
   const getJSONFromSheet = (e: ProgressEvent<FileReader>): Persona[] => {
     const bufferArray = e?.target?.result;
@@ -30,21 +30,25 @@ export default function Home() {
     return XLSX.utils.sheet_to_json(ws);
   };
 
-
-
-  const readExcel = async (e: ChangeEvent<HTMLInputElement>, buildFunction) => {
+  const readExcel = async (
+    e: ChangeEvent<HTMLInputElement>,
+    buildFunction: {
+      (rawData: Persona[]): void;
+      (rawData: Persona[]): void;
+      (arg0: Persona[]): void;
+    }
+  ) => {
     //init file reader
     const fileReader = new FileReader();
-    const targetFile = (e.target as HTMLInputElement)?.files?.[0]
+    const targetFile = (e.target as HTMLInputElement)?.files?.[0];
     if (targetFile) {
       fileReader.readAsArrayBuffer(targetFile);
     }
 
-
     fileReader.onload = async (e) => {
       const rawData: Persona[] = getJSONFromSheet(e);
       try {
-        await PersonasSchemaValidation.validate(rawData)
+        await PersonasSchemaValidation.validate(rawData);
         buildFunction(rawData);
 
         sendNotification({
@@ -60,8 +64,6 @@ export default function Home() {
     };
   };
 
-
-
   return (
     <>
       <Head>
@@ -71,34 +73,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-
-
-
         <Box>
-
-          <Typography variant="h4" mb={1.5}>
+          <Typography variant="h3" mb={1.5}>
             Sheet Schema
           </Typography>
 
-          <Image src={cellImage} alt="excel debe contener columnas id, nombre, apellido y monto" />
+          <Image
+            src={cellImage}
+            alt="excel debe contener columnas id, nombre, apellido y monto"
+          />
         </Box>
+<Box>  
+        <Typography variant="h4" mb={0.5}>
+          Goals
+        </Typography>
 
-
-        <Box>
-
-          <Typography variant="h4" mb={1.5}>
-            Goals:
-            <ul>
-              <li>Extract elements appearing more than once </li>
-              <li>Extract elements without an id</li>
-              <li> Show in sheet elements appearing more than once, taking into account: element.amount = total sum of amounts between elements sharing the same id
-              </li>
-              <li>Show in sheet elements with an empty id field. Style respective cells as red. </li>
-
-            </ul>
-          </Typography>
+        <ul>
+          <li>Extract elements appearing more than once </li>
+          <li>Extract elements without an id</li>
+          <li>
+            {" "}
+            Show  elements appearing more than once, taking into
+            account: element.amount = total sum of amounts between elements
+            sharing the same id
+          </li>
+          <li>
+            Show in sheet elements with an empty id field. Style respective
+            cells as red.{" "}
+          </li>
+        </ul>
         </Box>
-
         <LoadingButton
           sx={{
             minHeight: "51px",
@@ -119,43 +123,41 @@ export default function Home() {
             type="file"
           />
         </LoadingButton>
-
-
-
-
-        <Box>
-
-<Typography variant="h4" mb={1.5}>
-  Personal take: 
-  </Typography>
-  
-  
-<Typography variant="h5" mb={0.5}>
-  Management Requirements: 
-  </Typography>
-  <ul>
-    <li>
-  making sure users wont forget adding the id field</li>
-    <li>
-  if the same person is added more than once, sum their amount shared between elements with the same id 
-</li>
-  </ul>  
-
-  
-<Typography variant="h5" mb={0.5} mt={1.5} >
-  Steps
-  </Typography>
-   <ul>
-    <li>Show in sheet all the valid elements</li>
-    <li>Repeated elements should show only once, their amount is the total sum between elements sharing the same id</li>
-    <li>Elements with empty id should show in red, as a visual hint (warning)</li>
-
-     
-    
-    
-  </ul>
  
-</Box>
+          <Typography variant="h3" gutterBottom >
+            Extra
+          </Typography>
+        <Box>
+          <Typography variant="h4" mb={1.5}>
+            Personal take:
+          </Typography>
+
+          <Typography variant="h5" mb={0.5}>
+            Management Requirements:
+          </Typography>
+          <ul>
+            <li>making sure users wont forget adding the id field</li>
+            <li>
+              if the same person is added more than once, sum their amount
+              shared between elements with the same id
+            </li>
+          </ul>
+
+          <Typography variant="h5" mb={0.5} mt={1.5}>
+            Steps
+          </Typography>
+          <ul>
+            <li>Show in sheet all the valid elements</li>
+            <li>
+              Repeated elements should show only once, their amount is the total
+              sum between elements sharing the same id
+            </li>
+            <li>
+              Elements with empty id should show in red, as a visual hint
+              (warning)
+            </li>
+          </ul>
+        </Box>
 
         <LoadingButton
           sx={{
